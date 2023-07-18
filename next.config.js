7ae5abd1d825c908852543c419920d2f40419e51
jill <jillwunnie1213@gmail.com>
2023-07-18 11:16:34 +0800

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  basePath: '/next_page',
+  assetPrefix: '/next_page',
 
-module.exports = nextConfig
+};
+module.exports = nextConfig;
